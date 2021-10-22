590ea4ab30d1d46d5fd1878f2bbf6993802313fa
Artyom Davidovich <dsda_90@mail.ru>
2021-10-22 20:32:53 +0400

    Added deps.

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import "./styles/gilroy.scss";
+
 createApp(App).use(router).mount("#app");
