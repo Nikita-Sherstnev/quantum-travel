11094c0fd33d533db154bc6e8a3167da33cf70c9
stepan14041999 <stepan.14041999@gmail.com>
2021-10-22 20:52:10 +0400

    Add qboard-client to req

---
+++ server/sandulyaks/solver.py
@@ -0,0 +1,17 @@
+from qboard import Solver
+import numpy as np
+
+# Access parameters
+PARAMS = {
+    "remote_addr": "https://remote.qboard.tech", 
+    "access_key": "a0062a5e-d3ae-4a17-8499-f4d40358de94"
+}
+
+# Solver initialization
+s = Solver(mode="remote:simcim", params=PARAMS) 
+
+# QUBO matrix definition
+Q = np.random.randn(5,5)
+
+# Getting results
+spins, energy = s.solve_qubo(Q, timeout=30)
