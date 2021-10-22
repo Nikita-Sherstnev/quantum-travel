0e3b75be9ce2e12106ba6c66cc3aef9ee552da3e
stepan14041999 <stepan.14041999@gmail.com>
2021-10-22 23:55:36 +0400

    Add some files

--- server/sandulyaks/solver.py
+++ server/sandulyaks/solver.py
@@ -7,11 +7,12 @@ PARAMS = {
     "access_key": "a0062a5e-d3ae-4a17-8499-f4d40358de94"
 }
 
-# Solver initialization
-s = Solver(mode="remote:simcim", params=PARAMS) 
+if __name__ == '__main__':
+    # Solver initialization
+    s = Solver(mode="remote:simcim", params=PARAMS)
 
-# QUBO matrix definition
-Q = np.random.randn(5,5)
+    # QUBO matrix definition
+    Q = np.random.randn(5,5)
 
-# Getting results
-spins, energy = s.solve_qubo(Q, timeout=30)
+    # Getting results
+    spins, energy = s.solve_qubo(Q, timeout=30)
