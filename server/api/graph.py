922c5d0013654043661ba18b883020813f5987ae
Nikita-Sherstnev <nikita1248@yandex.ru>
2021-10-24 01:30:32 +0300

    post

--- server/api/graph.py
+++ server/api/graph.py
@@ -1,10 +1,55 @@
+import math
+import json
+
 from api import base
+from solver import qubo_tsp
+
+from qboard import Solver
+import numpy as np
+
+PARAMS = {
+    "remote_addr": "https://remote.qboard.tech",
+    "access_key": "pass"
+}
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 
 class Graph(base.ApiResource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.post_parser = base.RequestParser()
+        self.post_parser.add_argument('graph', location='json', required=True)
 
     def post(self):
-        pass
-        # here goes the method
+        args = self.post_parser.parse_args()
+        nodes = json.loads(args.graph)
+
+        n = 0
+        for i in nodes:
+            if i[0] > i[1] and i > n:
+                n = i[0]
+            elif i[1] > n:
+                n = i[1]
+        n += 1
+        # Инициализируем Solver
+        s = Solver(mode="remote:gurobi", params=PARAMS)
+
+        # Определяем матрицу QUBO
+        Q = qubo_tsp(nodes, n)
+
+        i_lower = np.tril_indices(25, -1)
+        Q[i_lower] = Q.T[i_lower]
+        spins, energy = s.solve_qubo(Q, timeout=30)
+
+        ans = list(chunks(list(map(int, spins.tolist())), 5))
+        res = []
+        
+        for a in ans:
+            res.append(a.index(0))
+
+        return res
---
+++ server/solver.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def qubo_tsp(array, metro_count) -> np.ndarray:
+    # Количество станций метро
+    n = metro_count
+    # Штраф за нарушение органичений
+    penalty_a = 1
+    penalty_b = 4
+
+    q = np.zeros((n, n, n, n))
+
+    # Каждая станция встречается один раз в цикле
+    for i in range(n):
+        q[i, :, i, :] = penalty_a * (np.ones((n, n)) - np.eye(n))
+
+    # Добавление весов
+    for el in array:
+        q[el[0], :, el[1], :] = penalty_b * el[2] * (np.ones((n, n)) - np.eye(n))
+
+    # Станция в j цикле не может встретиться в другом цикле с таким же номер
+    for i in range(n):
+        q[:, i, :, i] = penalty_a * (np.ones((n, n)) - np.eye(n))
+
+    # Раскрывает 4-матрицу в 2-матрицу
+    return q.reshape((n * n, n * n))
